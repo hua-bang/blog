e4fc1fa2121f79d1d1d0d8037ed1ded41b87786a
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-03-26 08:54:28 +0800

    feat(algorithms): longestCommonPrefix

---
+++ docs/algorithms/code/algorithms/longestCommonPrefix.ts
@@ -0,0 +1,25 @@
+function getLongestCommonPrefix(str1: string, str2: string) {
+  const length = Math.min(str1.length, str2.length);
+  let index = 0;
+  while (index < length && str1[index] === str2[index]) {
+    index++;
+  }
+  return str1.substring(0, index);
+}
+
+function longestCommonPrefix(strs: string[]): string {
+  if (strs.length === 0) {
+    return "";
+  }
+
+  let prefix: string = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    prefix = getLongestCommonPrefix(prefix, strs[i]);
+    if (prefix.length === 0) {
+      return "";
+    }
+  }
+
+  return prefix;
+}
