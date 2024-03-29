77966fcd22c4f0c07c5923acce6c0d15dabe8248
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-03-29 08:28:04 +0800

    feat(algorithms): inorderTraversal

---
+++ docs/algorithms/code/algorithms/inorderTraversal.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  if (!root) {
+    return [];
+  }
+
+  const result: number[] = [];
+  const stack: TreeNode[] = [];
+  let current = root;
+
+  while (current || stack.length) {
+    while (current) {
+      stack.push(current);
+      current = current.left;
+    }
+    current = stack.pop()!;
+    result.push(current.val);
+    current = current.right;
+  }
+
+  return result;
+}
