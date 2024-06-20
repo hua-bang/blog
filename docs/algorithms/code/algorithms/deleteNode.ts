06aacda0739f677973d33374a48ed2b130ba3076
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-06-20 22:34:10 +0800

    feat: delete node

---
+++ docs/algorithms/code/algorithms/deleteNode.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+function deleteNode(node: ListNode | null): void {
+  if (!node) {
+    return;
+  }
+
+  const nextNode = node.next;
+
+  node.val = nextNode.val;
+
+  node.next = nextNode.next;
+}
