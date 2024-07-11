4e1f6d28c56a739e8abe91b3ebe8b779bb2a7c23
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-07-11 08:00:57 +0800

    feat: hasCycle

--- docs/algorithms/code/algorithms/hasCycle.ts
+++ docs/algorithms/code/algorithms/hasCycle.ts
@@ -29,3 +29,38 @@ function hasCycle(head: ListNode | null): boolean {
 
   return false;
 }
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+  if (!head || !head.next) {
+    return false;
+  }
+
+  let slow = head.next,
+    fast = head.next.next;
+
+  while (slow && fast) {
+    if (slow === fast) {
+      return true;
+    }
+
+    slow = slow.next;
+    if (!fast.next) {
+      return false;
+    }
+    fast = fast.next.next;
+  }
+
+  return false;
+}
