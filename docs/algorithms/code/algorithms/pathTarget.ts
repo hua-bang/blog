bdb9edb2674a9023bab1579e07ef61b4ee91c0ba
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-07-30 23:04:37 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/pathTarget.ts
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function pathTarget(root: TreeNode | null, target: number): number[][] {
+  const result: number[][] = [];
+  const arr: number[] = [];
+
+  const dfs = (tree: TreeNode | null, curr: number, arr: number[]) => {
+    if (!tree) {
+      return;
+    }
+
+    const nextCurr = curr + tree.val;
+
+    arr.push(tree.val);
+    if (nextCurr === target && !tree.left && !tree.right) {
+      result.push([...arr]);
+      arr.pop();
+      return;
+    }
+
+    if (tree.left) {
+      dfs(tree.left, nextCurr, arr);
+    }
+
+    if (tree.right) {
+      dfs(tree.right, nextCurr, arr);
+    }
+    arr.pop();
+  };
+
+  dfs(root, 0, arr);
+  return result;
+}
