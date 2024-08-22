46a484dcdee6487cbade4a853ef0eef4489b2046
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-08-22 08:10:39 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/inventoryManagement3.ts
@@ -0,0 +1,19 @@
+function inventoryManagement(stock: number[]): number {
+  let res = stock[0];
+  let count = 1;
+
+  for (let i = 1; i < stock.length; i++) {
+    if (stock[i] === res) {
+      count++;
+      continue;
+    }
+
+    if (count === 1) {
+      res = stock[i];
+    } else {
+      count--;
+    }
+  }
+
+  return res;
+}
