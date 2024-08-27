0e4da81299030ba9e2c2239c1931fcd3d495f1a9
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-08-27 08:28:08 +0800

    feat: daily algo

--- docs/algorithms/code/algorithms/goodsOrder.ts
+++ docs/algorithms/code/algorithms/goodsOrder.ts
@@ -56,3 +56,33 @@ function goodsOrder(goods: string): string[] {
   backtrack(arr, 0, goods.length, perm);
   return res;
 }
+
+function goodsOrder(goods: string): string[] {
+  const res: string[] = [];
+  const arr = [...goods].sort();
+  const n = arr.length;
+  const visited = new Array(n).fill(false);
+  const perm: string[] = [];
+
+  const backtrack = (index: number) => {
+    if (index === n) {
+      res.push(perm.join(""));
+      return;
+    }
+
+    for (let i = 0; i < n; i++) {
+      if (visited[i] || (i > 0 && !visited[i - 1] && arr[i] === arr[i - 1])) {
+        continue;
+      }
+
+      visited[i] = true;
+      perm.push(arr[i]);
+      backtrack(index + 1);
+      visited[i] = false;
+      perm.pop();
+    }
+  };
+
+  backtrack(0);
+  return res;
+}
--- docs/algorithms/code/algorithms/wordPuzzle1.ts
+++ docs/algorithms/code/algorithms/wordPuzzle1.ts
@@ -42,3 +42,48 @@ function wordPuzzle(grid: string[][], target: string): boolean {
 
   return false;
 }
+
+function wordPuzzle(grid: string[][], target: string): boolean {
+  const m = grid.length,
+    n = grid[0].length;
+  const visited = new Array(m).fill(0).map(() => new Array(n).fill(0));
+
+  const backtracking = (wordIndex: number, i: number, j: number) => {
+    if (
+      i < 0 ||
+      i >= m ||
+      j < 0 ||
+      j >= n ||
+      visited[i][j] ||
+      grid[i][j] !== target[wordIndex]
+    ) {
+      return;
+    }
+
+    if (wordIndex === target.length - 1) {
+      return true;
+    }
+
+    visited[i][j] = true;
+
+    const res =
+      backtracking(wordIndex + 1, i + 1, j) ||
+      backtracking(wordIndex + 1, i, j + 1) ||
+      backtracking(wordIndex + 1, i - 1, j) ||
+      backtracking(wordIndex + 1, i, j - 1);
+
+    visited[i][j] = false;
+
+    return res;
+  };
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === target[0] && backtracking(0, i, j)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
