27ee1912b858d61615116020709659c2e971dcde
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-09-14 08:05:48 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/rotate4.ts
@@ -0,0 +1,20 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+  const n = matrix.length;
+
+  const matrixNew = new Array(n).fill(0).map(() => new Array(n).fill(0));
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      matrixNew[j][n - 1 - i] = matrix[i][j];
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      matrix[i][j] = matrixNew[i][j];
+    }
+  }
+}
