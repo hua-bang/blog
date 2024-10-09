a8327409f54f785752f5253fa57a5a76e4a5986b
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-10-09 21:56:07 +0800

    feat: daily algo

--- docs/algorithms/code/algorithms/sort/insertion-sort.ts
+++ docs/algorithms/code/algorithms/sort/insertion-sort.ts
@@ -1,15 +1,21 @@
-const insertionSort = (arr: number[]) => {
-  for (let i = 1; i < arr.length; i++) {
-    let base = arr[i];
+const insertionSort = (nums: number[]) => {
+  for (let i = 1; i < nums.length; i++) {
+    // 当前的值
+    let base = nums[i];
+
+    // 前序索引
     let j = i - 1;
-    while (j >= 0 && arr[j] > base) {
-      arr[j + 1] = arr[j];
+
+    // 如果前序的比他小，则需要做替换
+    while (j >= 0 && nums[j] > base) {
+      nums[j + 1] = nums[j];
       j--;
     }
-    arr[j + 1] = base;
+
+    nums[j + 1] = base;
   }
 };
 
-const arr = [4, 1, 3, 1, 5, 2];
-insertionSort(arr);
-console.log(arr);
+const arr2: number[] = [1, 4, 3, 2, 8, 7];
+insertionSort(arr2);
+console.log("arr2", arr2);
