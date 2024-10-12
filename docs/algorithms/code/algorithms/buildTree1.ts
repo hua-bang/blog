eb343564325133f619338abf602b165409f7b42a
hua_bang <47221942+hua-bang@users.noreply.github.com>
2024-10-12 08:24:55 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/buildTree1.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+  if (!preorder.length) {
+    return null;
+  }
+
+  const rootNodeNum = preorder[0];
+
+  const rootIndexInInOrder = inorder.findIndex((node) => node === rootNodeNum);
+
+  const rootNode = new TreeNode(inorder[rootIndexInInOrder]);
+  rootNode.left = buildTree(
+    preorder.slice(1, rootIndexInInOrder + 1),
+    inorder.slice(0, rootIndexInInOrder)
+  );
+
+  rootNode.right = buildTree(
+    preorder.slice(rootIndexInInOrder + 1),
+    inorder.slice(rootIndexInInOrder + 1)
+  );
+
+  return rootNode;
+}
---
+++ docs/algorithms/code/algorithms/flatten1.ts
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify root in-place instead.
+ */
+function flatten(root: TreeNode | null): void {
+  if (!root) {
+    return null;
+  }
+
+  const stack: TreeNode[] = [];
+  const res: TreeNode[] = [];
+
+  let curr = root;
+  while (stack.length || curr) {
+    while (curr) {
+      stack.push(curr);
+      res.push(curr);
+      curr = curr.left;
+    }
+    curr = stack.pop().right;
+  }
+
+  for (let i = 0; i < res.length; i++) {
+    res[i].left = null;
+    res[i].right = res[i + 1] || null;
+  }
+}
