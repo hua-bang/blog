4ebe430081f42df9a3c57963fd1c586ba4928d46
huahua <huabang001124@gmail.comm>
2024-11-12 08:39:02 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/groupAnagrams2.ts
@@ -0,0 +1,14 @@
+function groupAnagrams(strs: string[]): string[][] {
+  const map: Record<string, string[]> = {};
+
+  for (let str of strs) {
+    const key = [...str].sort().join("");
+
+    if (!map[key]) {
+      map[key] = [];
+    }
+    map[key].push(str);
+  }
+
+  return Object.values(map);
+};
---
+++ docs/algorithms/code/algorithms/longestConsecutive2.ts
@@ -0,0 +1,25 @@
+function longestConsecutive(nums: number[]): number {
+  if (nums.length === 0) {
+    return 0;
+  }
+  nums.sort((a, b) => a - b);
+  const set = new Set(nums);
+
+  let max = 0;
+
+  for (let num of set) {
+    if (set.has(num - 1)) {
+      continue;
+    }
+
+    let currentNum = num;
+    while (set.has(currentNum + 1)) {
+      currentNum += 1;
+    }
+
+    max = Math.max(max, currentNum - num + 1);
+  }
+
+  return max;
+
+};
