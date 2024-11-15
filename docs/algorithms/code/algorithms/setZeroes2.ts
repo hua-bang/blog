ce1aa8755e90c4c2af3d64f98d0dcbf3e92d9f6b
huahua <huabang001124@gmail.comm>
2024-11-15 21:50:44 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/setZeroes2.ts
@@ -0,0 +1,29 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+  let cols: number[] = [], rows: number[] = [];
+  const m = matrix.length, n = matrix[0].length;
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (matrix[i][j] === 0) {
+        rows.push(i);
+        cols.push(j);
+      }
+    }
+  }
+
+  for (let col of cols) {
+    for (let i = 0; i < m; i++) {
+      matrix[i][col] = 0;
+    }
+  }
+
+  for (let row of rows) {
+    for (let i = 0; i < n; i++) {
+      matrix[row][i] = 0;
+    }
+  }
+
+};
