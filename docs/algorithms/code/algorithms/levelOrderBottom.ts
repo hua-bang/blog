bdb050d49e821f3a8ad8fe7ef1f5120fc117bf64
huahua <huabang001124@gmail.comm>
2025-01-02 08:39:06 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/levelOrderBottom.ts
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+  if (!root) {
+    return [];
+  }
+
+  const res: number[][] = [];
+  const stack: TreeNode[] = [root];
+
+  while (stack.length) {
+    let size = stack.length;
+    const levelRes: number[] = [];
+
+    while (size--) {
+      const node = stack.shift();
+      levelRes.push(node.val);
+
+      if (node.left) {
+        stack.push(node.left);
+      }
+
+      if (node.right) {
+        stack.push(node.right);
+      }
+    }
+    res.unshift(levelRes);
+  }
+
+  return res;
+};
---
+++ docs/algorithms/code/algorithms/postorderTraversal.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+  if (!root) {
+    return [];
+  }
+
+  const stack: TreeNode[] = [];
+  const res: number[] = [];
+
+  while (stack.length || root) {
+    while (root) {
+      res.push(root.val);
+      stack.push(root);
+      root = root.right;
+    }
+
+    root = stack.pop().left;
+  }
+
+  return res.reverse();
+};
