f0ffa969f3a8465c78b186627eb94bbfd49720c4
huahua <huabang001124@gmail.comm>
2025-01-17 22:43:08 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/combine3.ts
@@ -0,0 +1,20 @@
+function combine(n: number, k: number): number[][] {
+  const res: number[][] = [];
+  const arr: number[] = [];
+
+  const backtrack = (index: number) => {
+    if(arr.length === k) {
+      res.push([...arr]);
+      return;
+    }
+
+    for(let i = index; i <= n; i++) {
+      arr.push(i);
+      backtrack(i + 1);
+      arr.pop();
+    }
+  } 
+
+  backtrack(1); 
+  return res;
+};
