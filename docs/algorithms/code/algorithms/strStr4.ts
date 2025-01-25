c48ce8034c40b908150de3e03b8914c8581047c5
huahua <huabang001124@gmail.comm>
2025-01-25 21:42:00 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/strStr4.ts
@@ -0,0 +1,22 @@
+function strStr(haystack: string, needle: string): number {
+  const needleLength = needle.length, haystackLength = haystack.length;
+  if(needleLength > haystackLength) {
+    return -1;
+  }
+
+  for(let i = 0; haystackLength - i >= needleLength; i++) {
+    let index = i, index2 = 0;
+    while(index2 < needleLength && haystack[index] === needle[index2]) {
+      index++;
+      index2++
+    }
+
+    if(index2 >= needleLength) {
+      return i;
+    } else {
+      continue;
+    }
+  }
+
+  return -1;
+};
