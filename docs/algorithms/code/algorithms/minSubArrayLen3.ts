2639fe205ecd5f001acfa5b43ad61cc66f952291
huahua <huabang001124@gmail.comm>
2025-02-03 20:33:30 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/minSubArrayLen3.ts
@@ -0,0 +1,17 @@
+function minSubArrayLen(target: number, nums: number[]): number {
+  let min: number = Infinity;
+  let left = 0, right = 0, sum = 0;
+
+  while(right < nums.length) {
+    sum += nums[right];
+    while(sum >= target) {
+      min = Math.min(min, right - left + 1);
+      sum -= nums[left];
+      left++;
+    }
+
+    right++;
+  }
+
+  return min === Infinity ? 0 : min;
+};
