6901d95a5209308233bbd0a517046e8bc2cff24e
huahua <huabang001124@gmail.comm>
2025-02-15 20:39:21 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/search1.go
@@ -0,0 +1,20 @@
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := (left + right) / 2
+
+		curr := nums[mid]
+
+		if curr == target {
+			return mid
+		} else if curr > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return -1
+}
