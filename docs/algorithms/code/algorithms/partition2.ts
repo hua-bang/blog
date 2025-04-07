1dbb4851b0c7fa57be9c2fdcac0b9f9b2a39d21d
Jasper <huabang001124@gmail.com>
2025-04-07 08:48:53 +0800

    feat(algorithms): add backtracking solutions for subsets, partitions, and IP restoration

--- docs/algorithms/code/algorithms/partition2.ts
+++ docs/algorithms/code/algorithms/partition2.ts
@@ -1,3 +1,6 @@
+// 思路：通过回溯算法，枚举所有可能的分割方案，判断每个分割方案是否为回文串，如果是，则将其加入结果集中。
+// 时间复杂度：O(2^n * n), 其中2^n是因为切割方案有 2 ^ n - 1 种，每个分割方案需要 O(n) 的时间复杂度判断是否为回文串。
+// 空间复杂度：O(2^n * n), 其中2^n是 path 需要存储的所有分割方案，而 n 是调用栈的深度。
 function isPalindrome(s: string, left: number, right: number): boolean {
   while (left < right) {
     if (s[left] !== s[right]) {
@@ -13,13 +16,13 @@ function partition(s: string): string[][] {
   const res: string[][] = [];
   const path: string[] = [];
   const backtrack = (startIndex: number) => {
-    if(startIndex >= s.length) {
+    if (startIndex >= s.length) {
       res.push([...path]);
       return;
     }
 
-    for(let i = startIndex; i < s.length; i++) {
-      if(!isPalindrome(s, startIndex, i)) {
+    for (let i = startIndex; i < s.length; i++) {
+      if (!isPalindrome(s, startIndex, i)) {
         continue;
       }
       path.push(s.slice(startIndex, i + 1));
