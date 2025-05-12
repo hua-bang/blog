4c27d9c3bc4ee7aab8aa35312fc1ef09897d7b4b
Jasper <huabang001124@gmail.com>
2025-05-12 08:26:45 +0800

    feat: algo

---
+++ docs/algorithms/code/algorithms/numTrees1.ts
@@ -0,0 +1,22 @@
+// 思路：采用动态规划。
+// 定义状态： dp[i] 由 i 个节点组成的二叉搜索树的个数
+// 状态转移方程： dp[i] = dp[j - 1] * dp[i - j]， j 从 1 到 i 遍历
+// 初始状态： dp[0] = 1, dp[1] = 1
+// 最终状态： dp[n]
+function numTrees(n: number): number {
+  if (n <= 1) {
+    return 1;
+  }
+
+  const dp = new Array(n + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      dp[i] += dp[j - 1] * dp[i - j];
+    }
+  }
+
+  return dp[n];
+}
