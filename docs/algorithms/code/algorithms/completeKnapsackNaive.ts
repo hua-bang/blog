19f28cc385cf126a24e4168242c9f1d314a7d2bd
Jasper <huabang001124@gmail.com>
2025-05-25 11:20:36 +0800

    feat: 完全背包

--- docs/algorithms/code/algorithms/completeKnapsackNaive.ts
+++ docs/algorithms/code/algorithms/completeKnapsackNaive.ts
@@ -3,6 +3,7 @@
 // 状态定义：dp[i][j] 表示前 i 个物品在容量为 j 所能获得的最大价值
 // 状态转移方程：dp[i][j] = max(dp[i][j], dp[i - 1][j - weight[i] * k] + value[i] * k)
 // 复杂度分析：时间复杂度 O(n * w^2), 空间复杂度 O(n * w)
+// 时间复杂度详解：外层循环 O(n)，中层循环 O(w)，内层 k 循环最多执行 w 次（当 weight[i] = 1 时），所以总体是 O(n * w * w) = O(n * w^2)
 function completeKnapsackNaive(n: number, w: number, weight: number[], value: number[]): number {
   const dp = new Array(n).fill(0).map(() => new Array(w + 1).fill(0));
 
