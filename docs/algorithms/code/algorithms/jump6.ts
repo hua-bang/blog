05ecd3b4244d1afa91027927a26b3f548d8becd2
Jasper <huabang001124@gmail.com>
2025-07-17 08:53:37 +0800

    feat: daily algo

---
+++ docs/algorithms/code/algorithms/jump6.ts
@@ -0,0 +1,24 @@
+function jump(nums: number[]): number {
+  let count = 0, index = 0;
+  const n = nums.length;
+
+  while (index < n - 1) {
+    if (index + nums[index] >= n - 1) {
+      return count + 1;
+    }
+
+    let leftIndex = index + 1, rightIndex = index + nums[index];
+    let max = 0;
+    for (let i = leftIndex; i <= rightIndex; i++) {
+      if (max <= i + nums[i]) {
+        max = i + nums[i];
+        leftIndex = i;
+      }
+    }
+
+    index = leftIndex;
+    count++;
+  }
+
+  return count;
+};
